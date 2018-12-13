94e3f365ad21765eb89a72f7e09d6fa6512b8ea7
BloodBaron <barontm@gmail.com>
2018-12-13 09:32:39 +0100

    Limit - ladowanie ustawien przy zmianie on/off

--- src/Fakturowanie/PanelLimit.java
+++ src/Fakturowanie/PanelLimit.java
@@ -35,8 +35,8 @@ import javax.swing.text.DateFormatter;
 import javax.swing.text.InternationalFormatter;
 import javax.swing.text.MaskFormatter;
 
-public class PanelLimit extends JPanel{
-	
+public class PanelLimit extends JPanel {
+
 	private JLabel tytul;
 	private SwitchBox wlacznik;
 	private Font labelFont;
@@ -63,18 +63,18 @@ public class PanelLimit extends JPanel{
 	private JScrollPane listaFakturScroll;
 	private JComboBox<String> limitNetBrut;
 	private DefaultTableModel modelListyFaktur;
-	
-	public PanelLimit () {
+
+	public PanelLimit() {
 		super();
 		this.setBounds(260, 0, 740, 680);
 		this.setLayout(null);
 		this.setBackground(Color.CYAN);
-		
+
 		tytul = new JLabel("LIMIT");
 		tytul.setFont(new Font("TimesRoman", Font.BOLD, 30));
 		tytul.setBounds(120, 20, 500, 40);
 		tytul.setHorizontalAlignment(SwingConstants.CENTER);
-		
+
 		labelFont = new Font("TimesRoman", Font.CENTER_BASELINE, 20);
 
 		wlacznikLab = new JLabel("Limit Wł/Wył");
@@ -83,11 +83,11 @@ public class PanelLimit extends JPanel{
 		trybLab = new JLabel("Tryb limitu");
 		trybLab.setFont(labelFont);
 		trybLab.setBounds(30, 150, 150, 30);
-		
+
 		wlacznik = new SwitchBox("ON", "OFF");
 		wlacznik.setBounds(400, 100, 130, 30);
 		wlacznik.setSelected(true);
-		
+
 		trybAutomatyczny = new JRadioButton("Tryb automatyczny");
 		trybAutomatyczny.setFont(new Font("TimesRoman", Font.CENTER_BASELINE, 15));
 		trybAutomatyczny.setBounds(400, 150, 200, 30);
@@ -97,52 +97,53 @@ public class PanelLimit extends JPanel{
 		trybyGrupa = new ButtonGroup();
 		trybyGrupa.add(trybReczny);
 		trybyGrupa.add(trybAutomatyczny);
-		trybAutomatyczny.setSelected(true);		
+		trybAutomatyczny.setSelected(true);
 
 		zastosuj = new JButton("ZASTOSUJ");
 		zastosuj.setBounds(550, 620, 150, 30);
-		
-		//------- panel trybow limitu
-		
+
+		// ------- panel trybow limitu
+
 		panelParametrow = new JPanel();
 		panelParametrow.setLayout(null);
 		panelParametrow.setBounds(0, 230, 740, 370);
 		panelParametrow.setBackground(Color.MAGENTA);
 		panelParametrow.setVisible(true);
-		
+
 		kwotaLab = new JLabel("Kwota limitu");
 		kwotaLab.setFont(labelFont);
 		kwotaLab.setBounds(30, 80, 150, 30);
-		
+
 		kwotaTxt = new JFormattedTextField();
 		kwotaTxt.setFont(trybAutomatyczny.getFont());
 		kwotaTxt.setBounds(400, 80, 120, 30);
 		kwotaTxt.setHorizontalAlignment(JTextField.CENTER);
 		kwotaTxt.setFormatterFactory(new AbstractFormatterFactory() {
-            @Override
-            public AbstractFormatter getFormatter(JFormattedTextField tf) {
-                NumberFormat format = DecimalFormat.getInstance();
-                format.setMinimumFractionDigits(2);
-                format.setMaximumFractionDigits(2);
-                format.setRoundingMode(RoundingMode.HALF_UP);
-                InternationalFormatter formatter = new InternationalFormatter(format);
-                formatter.setAllowsInvalid(false);
-                formatter.setMinimum(0.0);
-                formatter.setMaximum(10000000.00);
-                return formatter;
-            }});
+			@Override
+			public AbstractFormatter getFormatter(JFormattedTextField tf) {
+				NumberFormat format = DecimalFormat.getInstance();
+				format.setMinimumFractionDigits(2);
+				format.setMaximumFractionDigits(2);
+				format.setRoundingMode(RoundingMode.HALF_UP);
+				InternationalFormatter formatter = new InternationalFormatter(format);
+				formatter.setAllowsInvalid(false);
+				formatter.setMinimum(0.0);
+				formatter.setMaximum(10000000.00);
+				return formatter;
+			}
+		});
 		kwotaTxt.setText(Float.toString(10000.00f));
-		
+
 		limitNetBrut = new JComboBox<String>(Statyczne.getUstawienia().getRodzajeLimitu());
 		limitNetBrut.setFont(trybAutomatyczny.getFont());
 		limitNetBrut.setBounds(520, 80, 100, 30);
-						
-		//------- tryb automatyczny
-		
+
+		// ------- tryb automatyczny
+
 		autosumLab = new JLabel("Sprawdzaj limit sumując");
 		autosumLab.setFont(labelFont);
 		autosumLab.setBounds(20, 0, 300, 30);
-		
+
 		biezacyMiesiac = new JRadioButton("Bieżący miesiąc");
 		biezacyMiesiac.setFont(trybAutomatyczny.getFont());
 		biezacyMiesiac.setBounds(390, 0, 200, 30);
@@ -153,13 +154,13 @@ public class PanelLimit extends JPanel{
 		biezacyGrupa.add(biezacyMiesiac);
 		biezacyGrupa.add(biezacyRok);
 		biezacyMiesiac.setSelected(true);
-		
-		//------- tryb reczny
-		
+
+		// ------- tryb reczny
+
 		mansumLab = new JLabel("Sprawdzaj limit od dnia");
 		mansumLab.setFont(labelFont);
 		mansumLab.setBounds(20, 0, 300, 30);
-		
+
 		try {
 			dataTxt = new JFormattedTextField(new MaskFormatter("##-##-####"));
 		} catch (ParseException e) {
@@ -170,20 +171,8 @@ public class PanelLimit extends JPanel{
 		dataTxt.setBounds(390, 0, 120, 30);
 		dataTxt.setHorizontalAlignment(JTextField.CENTER);
 		dataTxt.setText("07-12-2018");
-		dataTxt.setFormatterFactory(new AbstractFormatterFactory() {
-			@Override
-			public AbstractFormatter getFormatter(JFormattedTextField tf) {
-				SimpleDateFormat format = (SimpleDateFormat) SimpleDateFormat.getDateInstance(3);
-				format.format(date);
-				InternationalFormatter formatter = new InternationalFormatter(format);
-				formatter.setAllowsInvalid(false);
-				formatter.setMinimum(0.0);
-				formatter.setMaximum(1000000.00);
-				return formatter;
-			}
-		});
-		
-		//------- lista
+
+		// ------- lista
 		modelListyFaktur = new DefaultTableModel(TabelaFaktur.getNazwyKolumn(), 0) {
 			@Override
 			public boolean isCellEditable(int row, int column) {
@@ -195,37 +184,36 @@ public class PanelLimit extends JPanel{
 		listaFakturScroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
 		listaFakturScroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 		listaFaktur.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		
+
 		podgladFaktury = new JButton("PODGLAD FAKTURY");
 		podgladFaktury.setBounds(475, 330, 225, 30);
-
 
 		panelPodListeFaktur = new JPanel();
 		panelPodListeFaktur.setLayout(new BorderLayout());
 		panelPodListeFaktur.add(listaFakturScroll, BorderLayout.CENTER);
 		panelPodListeFaktur.setBounds(30, 130, 680, 200);
-		
-		//------- panele trybu
-				
+
+		// ------- panele trybu
+
 		panelAutomatyczny = new JPanel();
 		panelAutomatyczny.setLayout(null);
 		panelAutomatyczny.setBounds(10, 0, 720, 60);
 		panelAutomatyczny.setBackground(Color.GREEN);
 		panelAutomatyczny.setVisible(true);
-		
+
 		panelAutomatyczny.add(biezacyMiesiac);
 		panelAutomatyczny.add(biezacyRok);
 		panelAutomatyczny.add(autosumLab);
-		
+
 		panelReczny = new JPanel();
 		panelReczny.setLayout(null);
 		panelReczny.setBounds(10, 0, 720, 60);
 		panelReczny.setBackground(Color.LIGHT_GRAY);
 		panelReczny.setVisible(false);
-		
+
 		panelReczny.add(mansumLab);
-		panelReczny.add(dataTxt);		
-		
+		panelReczny.add(dataTxt);
+
 		panelParametrow.add(panelAutomatyczny);
 		panelParametrow.add(panelReczny);
 		panelParametrow.add(kwotaLab);
@@ -233,8 +221,8 @@ public class PanelLimit extends JPanel{
 		panelParametrow.add(panelPodListeFaktur);
 		panelParametrow.add(podgladFaktury);
 		panelParametrow.add(limitNetBrut);
-		
-		//------- listenery
+
+		// ------- listenery
 		wlacznik.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mousePressed(MouseEvent e) {
@@ -243,8 +231,7 @@ public class PanelLimit extends JPanel{
 					trybReczny.setVisible(false);
 					trybAutomatyczny.setVisible(false);
 					panelParametrow.setVisible(false);
-				}
-				else {
+				} else {
 					trybLab.setVisible(true);
 					trybReczny.setVisible(true);
 					trybAutomatyczny.setVisible(true);
@@ -268,7 +255,7 @@ public class PanelLimit extends JPanel{
 		zastosuj.addActionListener(l -> {
 			zastosuj();
 		});
-		
+
 		trybAutomatyczny.addChangeListener(l -> {
 			if (trybAutomatyczny.isSelected()) {
 				panelReczny.setVisible(false);
@@ -279,7 +266,6 @@ public class PanelLimit extends JPanel{
 			}
 		});
 
-				
 		this.add(tytul);
 		this.add(wlacznikLab);
 		this.add(trybLab);
@@ -289,29 +275,100 @@ public class PanelLimit extends JPanel{
 		this.add(panelParametrow);
 		this.add(zastosuj);
 	}
-	
+
 	public void zastosuj() {
 		Statyczne.getUstawienia().setLimitOn(wlacznik.isSelected());
-		if (trybAutomatyczny.isSelected()) {
-			Statyczne.getUstawienia().setTrybLimitu(1);
-		} else if (trybReczny.isSelected()) {
-			Statyczne.getUstawienia().setTrybLimitu(2);
+		if (wlacznik.isSelected()) {
+			if (trybAutomatyczny.isSelected()) {
+				Statyczne.getUstawienia().setTrybLimitu(1);
+				if (biezacyMiesiac.isSelected()) {
+					Statyczne.getUstawienia().setSprawdzajSumujac(1);
+				} else if (biezacyRok.isSelected()) {
+					Statyczne.getUstawienia().setSprawdzajSumujac(2);
+				}
+			} else if (trybReczny.isSelected()) {
+				Statyczne.getUstawienia().setTrybLimitu(2);
+				String[] dateParts = dataTxt.getText().split("-");
+				try {
+					int day = Integer.parseInt(dateParts[0]);
+					int month = Integer.parseInt(dateParts[1]) - 1;
+					int year = Integer.parseInt(dateParts[2]);
+					if ((day < 1) || (day > 31) || (month < 0) || (month > 11) || (year < 0) || (year > 3000)) {
+						JOptionPane.showMessageDialog(this, "Niepoprawna data.", "Błąd", JOptionPane.ERROR_MESSAGE);
+					} else {
+						Date data = new Date(day, month, year);
+						Statyczne.getUstawienia().setDataLimitu(data);
+					}
+				} catch (Exception e) {
+					JOptionPane.showMessageDialog(this, "Niepoprawna data.", "Błąd", JOptionPane.ERROR_MESSAGE);
+					dataTxt.setText("01-01-2018");
+				}
+			}
+			float kwotaLimitu = 0.0f;
+			String kwotaStr = "";
+			String[] kwotaParts = kwotaTxt.getText().split(" ");
+			for (String s : kwotaParts) {
+				kwotaStr += s;
+			}
+			String[] kwotaPartsCom = kwotaStr.split(",");
+			kwotaLimitu = Float.parseFloat(kwotaPartsCom[0]) + Float.parseFloat(kwotaPartsCom[1]) / 100;
+			Statyczne.getUstawienia().setLimit(kwotaLimitu);
+			Statyczne.getUstawienia().setLimitRodzaj((String) limitNetBrut.getSelectedItem());
 		}
-		if (biezacyMiesiac.isSelected()) {
-			Statyczne.getUstawienia().setSprawdzajSumujac(1);
-		} else if (biezacyRok.isSelected()) {
-			Statyczne.getUstawienia().setSprawdzajSumujac(2);
+		odswiezListeFaktur();
+	}
+
+	public void odswiezListeFaktur() {
+		Date data = new Date();
+		if (wlacznik.isSelected()) {
+			if (trybAutomatyczny.isSelected()) {
+				if (biezacyMiesiac.isSelected()) {
+					int month = data.getMonth();
+					int year = data.getYear();
+					data = new Date(year, month, 1);
+				} else if (biezacyRok.isSelected()) {
+					int year = data.getYear();
+					data = new Date(year, 0, 1);
+				}
+			} else if (trybReczny.isSelected()) {
+				String[] dateParts = dataTxt.getText().split("-");
+				try {
+					int day = Integer.parseInt(dateParts[0]);
+					int month = Integer.parseInt(dateParts[1]) - 1;
+					int year = Integer.parseInt(dateParts[2]);
+					if ((day < 1) || (day > 31) || (month < 0) || (month > 11) || (year < 0) || (year > 3000)) {
+						JOptionPane.showMessageDialog(this, "Niepoprawna data.", "Błąd", JOptionPane.ERROR_MESSAGE);
+					} else {
+						data = new Date(day, month, year);
+					}
+				} catch (Exception e) {
+					JOptionPane.showMessageDialog(this, "Niepoprawna data.", "Błąd", JOptionPane.ERROR_MESSAGE);
+					dataTxt.setText("01-01-2018");
+				}
+			}
+			
+			int j = 0;
+			for (Fakturka k : Statyczne.getHistoria().getFaktury()) {
+				if (k.getDataWystawienia().compareTo(data) <= 0) {
+					Object[] element = new Object[8];
+					element[0] = j + 1;
+					element[1] = k.getNrFaktury();
+					element[2] = SimpleDateFormat.getDateInstance(3).format(k.getDataWystawienia());
+					element[3] = Float.toString(k.getCenaKoncowaNetto());
+					element[4] = Float.toString(k.getCenaKoncowaBrutto());
+					element[5] = Statyczne.getUstawienia().getWaluta();
+					element[6] = k.isZamknieta();
+					element[7] = k.isUwzgledniona();
+					modelListyFaktur.addRow(element);
+					j++;
+				}
+			}
 		}
-		String[] dateParts = dataTxt.getText().split("-");
-		try {
-		Date data = new Date(Integer.parseInt(dateParts[0]), 
-				Integer.parseInt(dateParts[1]) - 1, 
-				Integer.parseInt(dateParts[2]));
-				Statyczne.getUstawienia().setDataLimitu(data);
-		} catch (Exception e) {
-			JOptionPane.showMessageDialog(this, "Niepoprawna data.", "Błąd", JOptionPane.ERROR_MESSAGE);
-		}
-		
+	}
+	
+	public void zaladujUstawieniaOnOff() {
+		if (Ustawienia.get)
+			// wczytywanie ustawien limitu po zmianie on/off
 	}
 
 }
